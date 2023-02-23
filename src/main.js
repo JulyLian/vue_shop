6ff45a60507da4a3ffc160a0829f81aaf5584616
连L <1980845184@qq.com>
2023-02-23 11:45:32 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+//导入全局样式表
+import './assets/css/global.css'
+//按需引入
+import { Form,FormItem,Input,Button,Message } from 'element-ui'
+//导入axios
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//把包挂载到vue原型对象上
+Vue.prototype.$http = axios  //每一个vue组件都可以通过this直接访问到$http,从而发起ajax请求
+
 
 Vue.config.productionTip = false
 
+//应用element-ui
+Vue.component(Form.name, Form)   //不把Form.name写进去，不会报错，但效果也不显示。Form.name是给组件取的名
+Vue.component(FormItem.name, FormItem)
+Vue.component(Input.name, Input)
+Vue.component(Button.name, Button)
+Vue.prototype.$message = Message  //把弹框组件挂载到vue原型对象上
+
+//应用插件
+Vue.use(VueRouter)
+
 new Vue({
+  router:router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+//该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+//引入组件
+import Login from '../components/Login'
+import Home from '../components/Home'
+
+//创建并暴露一个路由器
+const router = new VueRouter({
+    routes:[
+        {
+            path: '/',
+            redirect: '/login'  //用户从'/'地址就可以重定向‘/login'地址
+        },
+        {
+            path: '/login',
+            component: Login  ////当用户访问/login地址的时候，通过component属性来指定要展示的组件
+        },
+        {
+            path: '/home',
+            component: Home  ////当用户访问/login地址的时候，通过component属性来指定要展示的组件
+        }
+    ]
+})
+
+//挂载路由导航守卫
+router.beforeEach((to, from, next)=>{
+    //to 将要访问的路径，from 代表从哪个路径跳转而来， next 是一个函数，表示放行
+    // next() 放行，  next('/login') 强制跳转
+    
+    if(to.path === '/login') return next()
+    //获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+export default router
